3bf2969dfbc20244ca9d59f7b1f760625bd99755
MarlonChi <marlonchiodelli@hotmail.com>
2025-05-21 20:49:47 -0300

    chore: multer config

---
+++ src/configs/upload.ts
@@ -0,0 +1,36 @@
+import multer from "multer";
+import path from "node:path";
+import crypto from "node:crypto";
+
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
+
+const MAX_FILE_SIZE = 3 * 1024 * 1024; // 3MB
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/pjpeg",
+  "image/png",
+  "image/gif",
+  "image/webp",
+];
+
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename: (req, file, cb) => {
+      const fileHash = crypto.randomBytes(10).toString("hex");
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return cb(null, fileName);
+    },
+  }),
+};
+
+export {
+  TMP_FOLDER,
+  UPLOADS_FOLDER,
+  MAX_FILE_SIZE,
+  ACCEPTED_IMAGE_TYPES,
+  MULTER,
+};
